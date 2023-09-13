9093a3209fb057e29372590f2247ea27dc7bfd4e
Md. Nayem Hossain <nayem.hossain7298@gmail.com>
2023-09-13 06:35:33 +0600

    mail error solved

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -1,28 +1,40 @@
 import { NextResponse } from "next/server";
+import nodemailer from 'nodemailer';
 
 export async function GET(req, res) { // SMTP-simple mail transfer protocol.
+    try {
+        // 1. Get Email Address
+        const { searchParams } = new URL(req.url);
+        let receiverEmail = searchParams.get('email');
 
-    // 1. Get Email Address
-    const { searchParams } = new URL(req.url);
-    let receiverEmail = searchParams.get('email')
+        // 2. Mail Transporter
+        const transporter = nodemailer.createTransport({
+            host: 'mail.teamrabbil.com',
+            port: 25,
+            secure: false,
+            auth: {
+                user: "info@teamrabbil.com",
+                pass: "~sR4[bhaC[Qs",
+            },
+            tls: {
+                rejectUnauthorized: false
+            },
+        });
 
-    // 2. Mail Transporter
-    let mailTransporter = nodemailer.createTransport({
-        host: '',
-        port: '',
-        secure: '',
-        auth: '',
-        tls: '',
-    })
+        // 3. Mail Options
+        let myEmail = {
+            form: "info@teamrabbil.com",
+            to: receiverEmail,
+            subject: "Testing Email",
+            text: "Hello! How are you ?",
+            html: <a href="https://mail.google.com" target="blank">Try Gmail</a>
+        }
 
-    // 3. Mail Options
-    let mailOptons = {
-        form: "",
-        to: "",
-        sub: "",
-        text: "",
+        // 4. Send Email
+        const result = await transporter.sendMail(myEmail);
+        return NextResponse.json({ result: result })
+
+    } catch (error) {
+        return NextResponse.json({ result: error })
     }
-
-    // 3. Results
-    const result = await mailTransporter.sendmail(mailOptons)
 }
