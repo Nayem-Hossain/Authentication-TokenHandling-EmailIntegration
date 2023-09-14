fed41626b9aa35c49b6036eb0411d6bf997b1fab
Md. Nayem Hossain <nayem.hossain7298@gmail.com>
2023-09-14 15:59:30 +0600

    email problem solved

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -1,40 +1,77 @@
 import { NextResponse } from "next/server";
 import nodemailer from 'nodemailer';
 
-export async function GET(req, res) { // SMTP-simple mail transfer protocol.
+export async function POST(req, res) { // SMTP-simple mail transfer protocol.
+    const data = await req.json()
+
+    let subject = data['subject'];
+    let email = data['email'];
+    let message = data['message'];
+
+    // 1. Get Email Address from params
+    /* const { searchParams } = new URL(req.url);
+    let receiverEmail = searchParams.get('email'); */
+
+    // 2. Mail Transporter
+
+    // let transporter = nodemailer.createTransport({
+    //     host: "mail.teamrabbil.com",
+    //     port: 25,
+    //     secure: false,
+    //     auth: {
+    //         user: "info@teamrabbil.com",
+    //         pass: "~sR4[bhaC[Qs",
+    //     },
+    //     tls: {
+    //         rejectUnauthorized: false,
+    //     },
+    // });
+
+    // 3. Mail Options
+
+    // let myEmail = {
+    //     from: "info@teamrabbil.com",
+    //     to: receiverEmail,
+    //     subject: "Testing Email",
+    //     text: "Hello! How are you ?",
+    //     html: "<p>See <a href='https://nodemailer.com/smtp/oauth2/'>Nodemailer OAuth2 Doc.</a></p>",
+    // }
+
+    /* According to OAuth2 => transporter and mailOptions format */
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.gmail.com",
+        secure: false, // true for "port: 465", false for other ports
+        auth: {
+            user: process.env.NODEMAIL_USER,
+            // pass: process.env.NODEMAIL_APP_PASS, // App password does not require when use type OAuth2 
+            type: "OAuth2",
+            clientId: process.env.NODEMAIL_OAUTH2_CLIENT_ID,
+            clientSecret: process.env.NODEMAIL_OAUTH2_CLIENT_SECRET,
+
+            accessToken: process.env.NODEMAIL_OAUTH2_ACCESS_TOKEN,
+            refreshToken: process.env.NODEMAIL_OAUTH2_REFRESH_TOKEN,
+            expires: 3599,
+        },
+    });
+
+    // According to OAuth2 mailOptions formatting
+    let myEmail = {
+        from: process.env.NODEMAIL_USER,
+        to: email,
+        subject: subject,
+        text: "",
+        html: `<p>${message}<br/><a href='https://nodemailer.com/smtp/oauth2/'>Nodemail OAuth2.</a></p>`,
+    }
+
+    // 4. Send Email
     try {
-        // 1. Get Email Address
-        const { searchParams } = new URL(req.url);
-        let receiverEmail = searchParams.get('email');
 
-        // 2. Mail Transporter
-        const transporter = nodemailer.createTransport({
-            host: "mail.teamrabbil.com",
-            port: 25,
-            secure: false,
-            auth: {
-                user: "info@teamrabbil.com",
-                pass: "~sR4[bhaC[Qs",
-            },
-            tls: {
-                rejectUnauthorized: false,
-            },
-        });
-
-        // 3. Mail Options
-        let myEmail = {
-            form: "info@teamrabbil.com",
-            to: receiverEmail,
-            subject: "Testing Email",
-            text: "Hello! How are you ?",
-            html: <a href="https://mail.google.com" target="blank">Try Gmail</a>
-        }
-
-        // 4. Send Email
-        const result = await transporter.sendMail(myEmail);
-        return NextResponse.json({ result: result })
+        await transporter.sendMail(myEmail);
+        return NextResponse.json({ message: "send email successfully" })
 
     } catch (error) {
-        return NextResponse.json({ result: error })
+        return NextResponse.json({ status: false, message: "sending email faild" })
     }
 }
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,24 +1,47 @@
 "use client";
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image'
-import user from '/public/default_user.png'
-import Link from 'next/link'
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import user from "/public/default_user.png";
+import Link from "next/link";
 
 const Profile = () => {
     const router = useRouter();
+    const [emailOptions, setEmailOptions] = useState({
+        subject: "",
+        email: "",
+        message: "",
+    });
+    const handleInputChange = (name, value) => {
+        setEmailOptions({ ...emailOptions, [name]: value });
+    };
 
     const handleLogout = async () => {
-        const res = await fetch('/api/loginAuth', {
-            method: 'GET',
+        const res = await fetch("/api/loginAuth", {
+            method: "GET",
         });
         const data = await res.json();
 
-        if (data['status'] === true) {
-            router.replace('/');
+        if (data["status"] === true) {
+            router.replace("/");
         }
-    }
+    };
+
+    const handleEmailSend = async (e) => {
+        e.preventDefault();
+        const res = await fetch("/api/email", {
+            method: 'POST',
+            body: JSON.stringify(emailOptions),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (res.ok) {
+            alert("send email successfully!")
+        }
+    };
 
     return (
         <div>
@@ -35,7 +58,9 @@ const Profile = () => {
                         <li><Link href="/contact">Contact</Link></li>
                     </ul>
                 </div> */}
-                    <label className="btn btn-ghost normal-case text-xl">JWT Token Auth</label>
+                    <label className="btn btn-ghost normal-case text-xl">
+                        JWT Token Auth
+                    </label>
                 </div>
                 {/*  <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -45,7 +70,7 @@ const Profile = () => {
                     <li><Link href={`/contact`}>Contact</Link></li>
                 </ul>
             </div> */}
-                <div className='navbar-end'>
+                <div className="navbar-end">
                     <div className="flex-none gap-2">
                         {/* <div className="form-control">
                     <input type="text" placeholder="Search blog" className="input input-bordered w-24 md:w-auto" />
@@ -53,26 +78,106 @@ const Profile = () => {
                         <div className="dropdown dropdown-end">
                             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                                 <div className="w-10 rounded-full">
-                                    <Image src={user} alt='user' />
+                                    <Image src={user} alt="user" />
                                 </div>
                             </label>
-                            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                            <ul
+                                tabIndex={0}
+                                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+                            >
                                 <li>
                                     <Link href={""} className="justify-between">
                                         Profile
                                         <span className="badge">New</span>
                                     </Link>
                                 </li>
-                                <li><Link href={""}>Settings</Link></li>
-                                <li><Link href={""} onClick={handleLogout}>Logout</Link></li>
+                                <li>
+                                    <Link href={""}>Settings</Link>
+                                </li>
+                                <li>
+                                    <Link href={""} onClick={handleLogout}>
+                                        Logout
+                                    </Link>
+                                </li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </nav>
-            <div>Profile page</div>
+            {/* contact form  */}
+            <div className="flex justify-end">
+                <div className="mt-8 lg:w-1/2 lg:mx-6">
+                    <div className="w-full px-8 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl dark:bg-[#191d24] lg:max-w-xl">
+                        <h1 className="text-2xl font-medium text-gray-700 dark:text-gray-200">
+                            Contact form
+                        </h1>
+
+                        <form onSubmit={handleEmailSend} className="mt-6">
+                            <div className="flex-1">
+                                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                                    Subject
+                                </label>
+                                <input
+                                    onChange={(e) => {
+                                        handleInputChange("subject", e.target.value);
+                                    }}
+                                    required
+                                    type="text"
+                                    name="subject"
+                                    value={emailOptions.subject}
+                                    placeholder="Enter email subject"
+                                    className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-[#191d24] dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                                />
+                            </div>
+
+                            <div className="flex-1 mt-6">
+                                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                                    Email address
+                                </label>
+                                <input
+                                    onChange={(e) => {
+                                        handleInputChange("email", e.target.value);
+                                    }}
+                                    required
+                                    type="email"
+                                    name="email"
+                                    value={emailOptions.email}
+                                    placeholder="Enter your email"
+                                    className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-[#191d24] dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                                />
+                            </div>
+
+                            <div className="w-full mt-6">
+                                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                                    Message
+                                </label>
+                                <textarea
+                                    onChange={(e) => {
+                                        handleInputChange("message", e.target.value);
+                                    }}
+                                    required
+                                    type="text"
+                                    name="message"
+                                    value={emailOptions.message}
+                                    className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-[#191d24] dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                                    placeholder="Message"
+                                ></textarea>
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gradient-to-r from-[#2361ff] to-[#07f7f7] rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50"
+                            >
+                                Send Email
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Profile;
+
+
